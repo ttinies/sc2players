78c7bf248b9c445182a43b23c65c2c172f4516ef
ttinies <ttinies.v@gmail.com>
2018-06-15 20:31:58 -0600

    - included exceptions exported from elsewhere

--- constants.py
+++ constants.py
@@ -9,6 +9,11 @@ from sc2common.types     import *
 import os
 
 ################################################################################
+class InvalidPlayerTypeException(Exception): pass
+class InvalidRaceException(      Exception): pass
+class InvalidDifficultyException(Exception): pass
+
+################################################################################
 PLAYERS_FOLDER      = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dataPlayers")
 DEFAULT_TIME_LIMIT  = 90 # days
 NO_ACTIVITY_LIMIT   = 10 # days
