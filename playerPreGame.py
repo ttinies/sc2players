5b781d99b87c1b718171a9d30bf2c67323d0e6fd
ttinies <ttinies.v@gmail.com>
2018-06-19 07:50:26 -0600

    - added playerID as an PlayerPreGame object variable for once the player joins the ga me - simplified control() method logic

--- playerPreGame.py
+++ playerPreGame.py
@@ -14,26 +14,32 @@ from sc2players import constants as c
 ################################################################################
 class PlayerPreGame(PlayerRecord):
     ############################################################################
-    def __init__(self, playerProfile, selectedRace=c.RANDOM, observe=False):
+    def __init__(self, playerProfile, selectedRace=c.RANDOM, observe=False, playerID=0):
         if isinstance(playerProfile, PlayerPreGame): # copy constructor (ignore other params)
             self.selectedRace   = playerProfile.selectedRace#c.SelectRaces(playerProfile.selectedRace.type)
             self.isObserver     = playerProfile.isObserver
+            self.playerID       = playerProfile.playerID
         else:
             self.selectedRace   = c.SelectRaces(selectedRace)
             self.isObserver     = observe
+            self.playerID       = playerID
         super(PlayerPreGame, self).__init__(source=playerProfile) # could also specify a player's profile name
     ############################################################################
     def __repr__(self):
         control = self.control()
-        if control == c.COMPUTER:   return super(PlayerPreGame, self).__repr__() # already declared
-        added = "" if self.isObserver else "%s "%(self.selectedRace.type)
+        #if control == c.COMPUTER:   return super(PlayerPreGame, self).__repr__() # already declared
+        added = ""
+        if not self.isObserver:
+            if self.playerID:
+                added += "#%d "%self.playerID
+            added += "%s "%(self.selectedRace.type)
         return "<%s %s%s %s-%s>"%(self.__class__.__name__,
             added, control.type, self.type.type, self.name)
     ############################################################################
-    #@property
     def control(self):
-        if   self.type == c.COMPUTER:   value = c.COMPUTER
-        elif self.isObserver:           value = c.OBSERVER
-        else:                           value = c.PARTICIPANT
+        """the type of control this player exhibits"""
+        if   self.isComputer:   value = c.COMPUTER
+        elif self.isObserver:   value = c.OBSERVER
+        else:                   value = c.PARTICIPANT
         return c.PlayerControls(value)
 
